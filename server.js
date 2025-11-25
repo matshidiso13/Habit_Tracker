b67150f4d0934fa1a9b0e04f6e5bb702b546251e
Thembeka <ttheh98@gmail.com>
2025-11-25 16:49:35 +0200

    new changes

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const port = 3000;
 const express = require("express");
 const app = express();
+const authRouter = require('./Middleware/auth')
 
 app.use(express.json());
-
+app.use('/api/auth', authRouter);
+app.use('/v1', require('./Middleware/users'))
 app.get("/",(req, res) =>{
     res.send("welcome to habit tracker")
 });
