4d8df099c367a1a8bcc129b29d1b7696eb2260eb
DA M1 34 <dam134@DAs-MacBook-Air-2.local>
2025-11-26 09:32:40 +0200

    added my app

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,5 +1,17 @@
-
+const express = require('express');
 const router = express.Router();
-router.get("/dashboard", authMiddleware, adminOnly, getDashboardData);
+const { adminOnly } = require('../Middleware/adminOnly');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
-export default router;
+router.patch("/tasks/:id/ban", adminOnly, async (req, res) => {
+    const taskId = parseInt(req.params.id);
+  
+    const task = await prisma.task.update({
+      where: { id: taskId },
+      data: { banned: true }
+    });
+  
+    res.json({ message: "Task banned successfully", task });
+  });
+  
