c1ad62823596ed301e24ae4fef97bd73df02af63
matshidiso13 <mbelediva@gmail.com>
2025-11-26 11:21:57 +0200

    prisma code

--- Routes/admin.js
+++ Routes/admin.js
@@ -4,6 +4,7 @@ const { adminOnly } = require('../Middleware/adminOnly');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+
 router.patch("/tasks/:id/ban", adminOnly, async (req, res) => {
     const taskId = parseInt(req.params.id);
   
